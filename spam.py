e7d443ce3c84a4d3f2f7f59317bf88561b3981aa
rahul rathore <you@example.com>
2022-11-05 17:04:15 +0530

    make it better

---
+++ spam.py
@@ -0,0 +1,39 @@
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from win32com.client import Dispatch
+import streamlit as st
+
+
+def speak(text):
+    speaks = Dispatch("SAPI.SpVoice")
+    speaks.Speak(text)
+
+
+model = pickle.load(open('spam.pkl', 'rb'))
+cv = pickle.load(open('vectorized.pkl', 'rb'))
+
+
+def main():
+    st.title("Email Spam Classification Application")
+    st.write("Build with Streamlit & Python")
+    activ = ["Classification", "About"]
+    choices = st.sidebar.selectbox("Select Activities", activ)
+    if choices == "Classification":
+        st.subheader("Classification")
+        msg = st.text_input("Enter a text")
+        if st.button("Process"):
+            print(msg)
+            print(type(msg))
+            data = [msg]
+            print(data)
+            vec = cv.transform(data).toarray()
+            result = model.predict(vec)
+            if result[0] == 0:
+                st.success("This is Not A Spam Email")
+                speak("This is Not A Spam Email")
+            else:
+                st.error("This is A Spam Email")
+                speak("This is A Spam Email")
+
+
+main()
